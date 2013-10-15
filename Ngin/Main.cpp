1003d0efd1b8b6fbdb9df9931e50a135224940c3
carlixyz <carlixyz@yahoo.com.ar>
2013-10-15 17:15:14 -0300

    Added 2D Image (Sprites) support
    - Some sprite position issue, creating them inside some walls
    - I already had a Sprite struct and slighty different inside so I just
    renamed this new struct as 'Image' in the end It's the same shit

--- Ngin/Main.cpp
+++ Ngin/Main.cpp
@@ -23,6 +23,51 @@
 
 Sprite Test;
 
+struct Image
+{
+  double x;
+  double y;
+  int texture;
+};
+
+#define numImages 19
+Image images[numImages] =
+{
+  {20.5, 11.5, 10}, //green light in front of playerstart
+  //green lights in every room
+  {18.5,4.5, 10},
+  {10.0,4.5, 10},
+  {10.0,12.5,10},
+  {3.5, 6.5, 10},
+  {3.5, 20.5,10},
+  {3.5, 14.5,10},
+  {14.5,20.5,10},
+
+  //row of pillars in front of wall: fisheye test
+  {18.5, 10.5, 9},
+  {18.5, 11.5, 9},
+  {18.5, 12.5, 9},
+
+  //some barrels around the map
+  {21.5, 1.5, 8},
+  {15.5, 1.5, 8},
+  {16.0, 1.8, 8},
+  {16.2, 1.2, 8},
+  {3.5,  2.5, 8},
+  {9.5, 15.5, 8},
+  {10.0, 15.1,8},
+  {10.5, 15.8,8},
+};
+//1D Zbuffer
+double ZBuffer[WIDTH];
+
+//arrays used to sort the sprites
+int imgOrder[numImages];
+double imgDistance[numImages];
+
+//function used to sort the sprites
+void combSort(int* order, double* dist, int amount);
+
 #define mapWidth 24
 #define mapHeight 24
 #define texWidth 64
@@ -58,6 +103,9 @@ int worldMap[mapWidth][mapHeight]=
 //DWORD buffer[WIDTH][HEIGHT];
 DWORD buffer[WIDTH*HEIGHT];
 //Uint32 buffer[WIDTH][HEIGHT];
+
+
+
 
 
 void UpdateInput(Vector2D & pos, Vector2D & dir, Vector2D & plane, float lfTimestep)
@@ -124,28 +172,9 @@ int main()
 
 	//std::vector<Uint32> textures[8];
 	//for(int i = 0; i < 8; i++) textures[i].resize(texWidth * texHeight);
-	DWORD textures[8][texWidth*texHeight];
+	DWORD textures[11][texWidth*texHeight];
 
-	bool success = ptc_open( "Raycaster", WIDTH, HEIGHT);
-
-	  //generate some textures
-	 //generate some textures
-	 // for(int x = 0; x < texWidth; x++)
-	 // for(int y = 0; y < texHeight; y++)
-	 // {
-		//int xorcolor = (x * 256 / texWidth) ^ (y * 256 / texHeight);
-		////int xcolor = x * 256 / texWidth;
-		//int ycolor = y * 256 / texHeight;
-		//int xycolor = y * 128 / texHeight + x * 128 / texWidth;
-		//textures[0][texWidth * y + x] = 65536 * 254 * (x != y && x != texWidth - y); //flat red texture with black cross
-		//textures[1][texWidth * y + x] = xycolor + 256 * xycolor + 65536 * xycolor; //sloped greyscale
-		//textures[2][texWidth * y + x] = 256 * xycolor + 65536 * xycolor; //sloped yellow gradient
-		//textures[3][texWidth * y + x] = xorcolor + 256 * xorcolor + 65536 * xorcolor; //xor greyscale
-		//textures[4][texWidth * y + x] = 256 * xorcolor; //xor green
-		//textures[5][texWidth * y + x] = 65536 * 192 * (x % 16 && y % 16); //red bricks
-		//textures[6][texWidth * y + x] = 65536 * ycolor; //red gradient
-		//textures[7][texWidth * y + x] = 128 + 256 * 128 + 65536 * 128; //flat grey texture
-	 // }
+	bool success = (ptc_open( "Raycaster", WIDTH, HEIGHT) > 0);
 
 	if(! gfx::loadImage(textures[0], "pics/eagle.png") ) success = false;
 	if(! gfx::loadImage(textures[1], "pics/redbrick.png") ) success = false;
@@ -156,6 +185,9 @@ int main()
 	if(! gfx::loadImage(textures[6], "pics/wood.png") ) success = false;
 	if(! gfx::loadImage(textures[7], "pics/colorstone.png") ) success = false;
 	
+	if(! gfx::loadImage(textures[8], "pics/barrel.png") ) success = false;
+	if(! gfx::loadImage(textures[9], "pics/pillar.png") ) success = false;
+	if(! gfx::loadImage(textures[10], "pics/greenlight.png") ) success = false;
 
 	if (! gfx::LoadSprite(Test, "calvin1.png")) success = false;
 	if(!success)
@@ -284,7 +316,133 @@ int main()
 				//buffer[x][y] = color;
 			}
 
+			      //SET THE ZBUFFER FOR THE SPRITE CASTING
+			ZBuffer[x] = perpWallDist; //perpendicular distance is used
+
+			// Floor casting
+			Vector2D floorWall = { 0, 0};
+
+			// 4 Wall projection posible 
+			if ( side == 0 && rayDir.x > 0)
+			{
+				floorWall.x = mapX;
+				floorWall.y = mapY + wallX ;
+			}
+			else if ( side == 0 && rayDir.x < 0)
+			{
+				floorWall.x = mapX + 1.0;
+				floorWall.y = mapY + wallX ;
+			}
+			else if ( side == 1 && rayDir.y > 0)
+			{
+				floorWall.x = mapX + wallX;
+				floorWall.y = mapY;
+			}
+			else
+			{
+				floorWall.x = mapX + wallX;
+				floorWall.y = mapY + 1.0;
+			}
+
+			double distWall = perpWallDist, distPlayer = 0, currentDist = 0;
+
+			if ( drawEnd < 0 ) drawEnd = HEIGHT; // becomes < 0 when the integer Overflows
+			// Draw the floor from drawEnd to the bottom of the Screen
+			for ( int y = drawEnd + 1; y < HEIGHT; y++ )
+			{
+				currentDist = HEIGHT / (2.0 * y - HEIGHT); //We could make a small lookup table for this instead
+
+				double weight = (currentDist - distPlayer) / (distWall - distPlayer);
+
+				Vector2D currentFloor = { weight * floorWall.x + (1.0 - weight) * pos.x, 
+										  weight * floorWall.y + (1.0 - weight) * pos.y };
+
+				int floorTexX = int(currentFloor.x * texWidth) % texWidth;
+				int floorTexY = int(currentFloor.y * texHeight) % texHeight;
+
+				//int checkerBoardPattern = (int(currentFloor.x) + int(currentFloor.y)) % 2;
+				//int floorTexture = 0;
+				//if(checkerBoardPattern == 0) floorTexture = 3;
+				//else floorTexture = 4;
+
+				//floor
+				buffer[y*WIDTH + x] = (textures[3][texWidth * floorTexY + floorTexX] >> 1) & 8355711;
+				//ceiling (symmetrical!)
+				buffer[(HEIGHT - y) *WIDTH + x] = textures[6][texWidth * floorTexY + floorTexX];
+			}
 		}
+
+		//SPRITE CASTING
+		//sort sprites from far to close
+		for(int i = 0; i < numImages; i++)
+		{
+		  imgOrder[i] = i;
+		  imgDistance[i] = ((pos.x - images[i].x) * (pos.x - images[i].x) + (pos.y - images[i].y) * (pos.y - images[i].y)); //sqrt not taken, unneeded
+		}
+		combSort(imgOrder, imgDistance, numImages);
+
+		   //after sorting the sprites, do the projection and draw them
+		for( int i = 0; i < numImages; i++ )
+		{
+		  //translate sprite position to relative to camera
+			Vector2D imgPos = { images[imgOrder[i]].x - pos.x, images[imgOrder[i]].y - pos.y};
+
+		  //transform sprite with the inverse camera matrix
+		  // [ planeX   dirX ] -1                                       [ dirY      -dirX ]
+		  // [               ]       =  1/(planeX*dirY-dirX*planeY) *   [                 ]
+		  // [ planeY   dirY ]                                          [ -planeY  planeX ]
+
+		  double invDet = 1.0 / (plane.x * dir.y - dir.x * plane.y); //required for correct matrix multiplication
+
+		  Vector2D transform = { invDet * (dir.y * imgPos.x - dir.x * imgPos.y), 
+									invDet * (-plane.y * imgPos.x + plane.x * imgPos.y) };
+		  //this is actually the depth inside the screen, that what Z is in 3D
+
+		  int spriteScreenX = int((WIDTH / 2) * (1 + transform.x / transform.y));
+
+		  //parameters for scaling and moving the sprites
+		  #define uDiv 1
+		  #define vDiv 1
+		  #define vMove 0.0
+		  int vMoveScreen = int(vMove / transform.y);
+
+		  //calculate height of the sprite on screen
+		  int spriteHeight = abs(int(HEIGHT / (transform.y))) / vDiv; //using "transformY" instead of the real distance prevents fisheye
+		  //calculate lowest and highest pixel to fill in current stripe
+		  int drawStartY = -spriteHeight / 2 + HEIGHT / 2 + vMoveScreen;
+		  if(drawStartY < 0) drawStartY = 0;
+		  int drawEndY = spriteHeight / 2 + HEIGHT / 2 + vMoveScreen;
+		  if(drawEndY >= HEIGHT) drawEndY = HEIGHT - 1;
+
+		  //calculate width of the sprite
+		  int spriteWidth = abs( int (HEIGHT / (transform.y))) / uDiv;
+		  int drawStartX = -spriteWidth / 2 + spriteScreenX;
+		  if(drawStartX < 0) drawStartX = 0;
+		  int drawEndX = spriteWidth / 2 + spriteScreenX;
+		  if(drawEndX >= WIDTH) drawEndX = WIDTH - 1;
+
+		  //loop through every vertical stripe of the sprite on screen
+		  for(int stripe = drawStartX; stripe < drawEndX; stripe++)
+		  {
+			int texX = int(256 * (stripe - (-spriteWidth / 2 + spriteScreenX)) * texWidth / spriteWidth) / 256;
+			//the conditions in the if are:
+			//1) it's in front of camera plane so you don't see things behind you
+			//2) it's on the screen (left)
+			//3) it's on the screen (right)
+			//4) ZBuffer, with perpendicular distance
+			if(transform.y > 0 && stripe > 0 && stripe < WIDTH && transform.y < ZBuffer[stripe])
+			for(int y = drawStartY; y < drawEndY; y++) //for every pixel of the current stripe
+			{
+			  int d = (y-vMoveScreen) * 256 - HEIGHT * 128 + spriteHeight * 128; //256 and 128 factors to avoid floats
+			  int texY = ((d * texHeight) / spriteHeight) / 256;
+			  Uint32 color = textures[images[imgOrder[i]].texture][texWidth * texY + texX]; //get current color from the texture
+			  //if((color & 0x00FFFFFF) != 0) buffer[y + stripe * HEIGHT] = color; //paint pixel if it isn't black, black is the invisible color
+			  if((color & 0x00FFFFFF) != 0) buffer[y * WIDTH+ stripe] = color; //paint pixel if it isn't black, black is the invisible color
+			}
+		  }
+		}
+
+
 
 		//gfx::drawBuffer(buffer[0]);
 		gfx::BufferDraw(buffer);
@@ -320,6 +478,30 @@ int main()
 	ExitProcess(0);
 }
 
+//sort algorithm
+void combSort(int* order, double* dist, int amount)
+{
+  int gap = amount;
+  bool swapped = false;
+  while(gap > 1 || swapped)
+  {
+    //shrink factor 1.3
+    gap = (gap * 10) / 13;
+    if(gap == 9 || gap == 10) gap = 11;
+    if (gap < 1) gap = 1;
+    swapped = false;
+    for (int i = 0; i < amount - gap; i++)
+    {
+      int j = i + gap;
+      if (dist[i] < dist[j])
+      {
+        std::swap(dist[i], dist[j]);
+        std::swap(order[i], order[j]);
+        swapped = true;
+      }
+    }
+  }
+}
 
 
 
